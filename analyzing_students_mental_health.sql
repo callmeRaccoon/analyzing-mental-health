b6cb722a5efdc63a42c1c4df62d0738f05bc48c1
Ferhat K. <121262217+callmeRaccoon@users.noreply.github.com>
2024-12-17 17:10:47 +0300

    Add files via upload

---
+++ analyzing_students_mental_health.sql
@@ -0,0 +1,8 @@
+SELECT stay, COUNT(inter_dom) AS count_int,
+	ROUND(AVG(todep), 2) AS average_phq,
+	ROUND(AVG(tosc), 2) AS average_scs,
+	ROUND(AVG(toas), 2) AS average_as
+FROM students
+WHERE stay IS NOT NULL AND inter_dom = 'Inter'
+GROUP BY stay
+ORDER BY stay DESC;
